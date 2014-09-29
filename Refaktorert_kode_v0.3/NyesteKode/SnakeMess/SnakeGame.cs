2250c1238b7ef22c21bb008395531d522dd7fbc7
IdarV <tjoida13@nith.no>
2014-09-29 15:38:21 +0200

    SnakeGame changes

---
+++ Refaktorert_kode_v0.3/NyesteKode/SnakeMess/SnakeGame.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+using System.Threading;
+
+namespace SnakeMess{
+
+    internal class SnakeGame
+    {
+        private screenController screen;
+        private GameMaster gm;
+        private int lastDirectionMoved, newDir, boardW, boardH;
+        private Snake snake;
+        private pellet pellet;
+        private Stopwatch timer;
+        private Boolean runGame;
+
+
+	    public SnakeGame() {
+			ShowScore = 0;
+	    }
+		// Skal være static method
+		public static int ShowScore { get; set; }
+
+		StreamWriter writer = new StreamWriter("test.txt");
+		public void RunGame(){
+
+	         // Boolean for running the game
+             runGame = true;
+
+            // Direction of movement
+           // newDir = 1; // 0 = up, 1 = right, 2 = down, 3 = left
+
+            // Gamemaster
+             gm = new GameMaster();
+
+            // Screen (output handler)
+            screen = new screenController();
+
+            // Get width and height from screen handler
+		    boardW = screen.getWidth();
+            boardH = screen.getHeight();
+
+            // Create snake
+            snake = new Snake();
+
+            // And pellet (Katrine: food)
+            pellet = new pellet(0, 0);
+
+            // Add 4 bodies to snake
+            snake.addBody(4, 10, 10);
+
+            // place pellet in world
+            pellet.placePellet(snake, boardH, boardW);
+
+            // Create a stopwatch for thread-waiting
+            timer = new Stopwatch();
+            timer.Start();
+
+            // Running the game
+            while (runGame){
+                // Change direction if key is pressed
+                newDir = gm.ReadKeys(lastDirectionMoved);
+                // Set direction of snake
+                snake.setDirection(newDir);
+
+                // newDir = 5 betyr pause. We are still in alpha
+                if (newDir != 5)
+                {
+                    // Wait 100 millis
+                    if (timer.ElapsedMilliseconds < 100)
+                    {
+                        continue;
+                    }
+                    // Restart counter
+                    timer.Restart();
+
+                    // Get new snakehead
+                    var newHead = snake.getNewHead();
+
+                    // Check if snake is eating pellet
+                    if (pellet.checkIfEatingPellet(snake))
+                    {
+	                    ShowScore++;
+                        // Grow snake
+                        snake.grow = true;
+                        // Place new pellet
+                        pellet.placePellet(snake, boardH, boardW);
+                    }
+
+                    // Check if snake eats himself, currenty disabled
+                    snake.checkSelfCannibalism(gm, newHead);
+                    if (gm.getGameOver())
+                    {
+                        break;
+                    }
+
+					// Kræsjer i boarder
+					/*if (snake.getHead().X <= 0 || snake.getHead().X >= boardW - 1 ||
+						snake.getHead().Y <= 0 || snake.getHead().Y >= boardH - 1)
+					{
+						break;
+					}*/
+                    runGame = snake.checkBoardCollision(boardH, boardW);
+
+                    // Update screen
+                    screen.updateScreen(snake, pellet, newHead);
+
+                    // Update last direction. Shark bois 4ever
+                    lastDirectionMoved = newDir;
+
+                }
+            }
+        }
+    }
+}
+		
+	
