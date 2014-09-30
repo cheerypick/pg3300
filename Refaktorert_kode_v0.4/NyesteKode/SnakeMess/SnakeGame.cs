b7acb7367765766e453413424a66f90596e63e26
IdarV <tjoida13@nith.no>
2014-09-30 12:42:19 +0200

    New version

---
+++ Refaktorert_kode_v0.4/NyesteKode/SnakeMess/SnakeGame.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+using System.Threading;
+
+namespace SnakeMess {
+
+    internal class SnakeGame {
+        private ScreenHandler screen;
+        private GameMaster gm;
+        private int lastDirectionMoved, newDir, boardW, boardH;
+        private Snake snake;
+        private pellet pellet;
+        private Stopwatch timer;
+        private Boolean runGame;
+        private Coord newHead;
+
+
+        public SnakeGame() {
+            ShowScore = 0;
+            // Boolean for running the game
+            runGame = true;
+
+            // Gamemaster
+            gm = new GameMaster();
+
+            // Screen (output handler)
+            screen = new ScreenHandler();
+
+            // Get width and height from screen handler
+            boardW = screen.getWidth();
+            boardH = screen.getHeight();
+
+            // Create snake
+            snake = new Snake();
+
+            // Create pellet
+            pellet = new pellet(0, 0);
+
+            // Add 4 bodies to snake
+            snake.addBody(4, 10, 10);
+
+            // place pellet in world
+            pellet.placePellet(snake, boardH, boardW);
+
+            // Create a stopwatch for thread-waiting
+            timer = new Stopwatch();
+            timer.Start();
+        }
+
+        // Skal være static method
+        public static int ShowScore { get; set; }
+
+       // StreamWriter writer = new StreamWriter("test.txt");
+        public void RunGame() {
+
+            /*		Rydd opp. Bør kanskje ikke være i denne klassen? Kanskje i en meny klasse?		*/
+           
+
+            // Running the game
+            while (runGame) {
+                
+                // Change direction if key is pressed
+                newDir = gm.ReadKeys(lastDirectionMoved);
+                // Set direction of snake
+                snake.setDirection(newDir);
+
+                // newDir = 5 betyr pause. We are still in alpha
+                if (newDir == 5) continue;
+                
+                // Wait 100 millis
+                if (timer.ElapsedMilliseconds < 100)  continue;
+                
+                // Restart counter
+                timer.Restart();
+
+                // Get new snakehead
+                newHead = snake.GetNewHead();
+
+                screen.DrawScore(ShowScore);
+
+                // Check if snake is eating pellet, do stuff if it is
+                CheckIfEatingPellet();
+
+                // Check if snake is crashing in border
+                if (CheckIfBorderCrash() || snake.CheckSelfCannibalism(gm, newHead)) break;
+
+                // Update screen
+
+                screen.updateScreen(snake, pellet, newHead);
+
+                // Update last direction. Shark bois 4ever
+                lastDirectionMoved = newDir;
+            }
+        }
+
+        // Check if snake is crashing in borders
+        public bool CheckIfBorderCrash() {
+            return snake.getHead().X <= 0 || snake.getHead().X >= boardW - 1 ||
+                   snake.getHead().Y <= 0 || snake.getHead().Y >= boardH - 1;
+        }
+
+        // Check is snake is eating pellet/apple
+        public void CheckIfEatingPellet() {
+            if (!pellet.checkIfEatingPellet(snake)) return;
+            ShowScore++;
+            // Grow snake
+            snake.grow = true;
+            // Place new pellet
+            pellet.placePellet(snake, boardH, boardW);
+        }
+    }
+}
---
+++ Refaktorert_kode_v0.4/NyesteKode/SnakeMess/gameMaster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess
+{
+    // Game master is master of gamez
+    public class GameMaster{
+        private ConsoleKeyInfo _inputKey;
+        private Boolean gameOver;
+
+	    public GameMaster()
+	    {
+		    
+	    }
+		
+	    // Method for reading keys
+        public int ReadKeys(int lastDirection)  {
+
+            // If key is pressed, read it
+            if (Console.KeyAvailable)
+                _inputKey = Console.ReadKey(true);
+
+
+                // Return diffrent ints for different inputs from different keys, different
+                switch (_inputKey.Key){
+                        /*  case ConsoleKey.Escape: // THIS IS FOR ESCAPE DO NOT REMOVE ME
+                    return 4;*/
+                    case ConsoleKey.Spacebar:
+                        return 5;
+                    case ConsoleKey.UpArrow: // 0
+                        return lastDirection != 2 ? 0 : lastDirection;
+
+                    case ConsoleKey.RightArrow: // 1
+                        return lastDirection != 3 ? 1 : lastDirection;
+
+                    case ConsoleKey.DownArrow: // 2
+                        return lastDirection != 0 ? 2 : lastDirection;
+
+                    case ConsoleKey.LeftArrow: // 3
+                        return lastDirection != 1 ? 3 : lastDirection;
+
+                    default:
+                        return lastDirection;
+
+
+                }
+        }
+
+        // Set game over
+        public void setGameOver(Boolean gameState){
+
+            this.gameOver = gameState;
+            //Environment.Exit(1); // FOR EXITING GAME
+        }
+
+	    public bool getGameOver()
+	    {
+		    return gameOver;
+	    }
+    }
+ }
+
+
+
