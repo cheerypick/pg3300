25bde339cf4b116fcb6eb32163db56eda6d34e41
ladyalpineblue <katrineorlova@gmail.com>
2014-10-02 15:20:25 +0200

    new folder

---
+++ Refaktorert_kode_v1.1/NyesteKode/SnakeMess/Coord.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SnakeMess {
+
+        public class Coord {
+
+        public int X;
+        public int Y;
+
+        public Coord(int x = 0, int y = 0) {
+            X = x;
+            Y = y;
+        }
+
+        public Coord(Coord input) {
+            X = input.X;
+            Y = input.Y;
+        }
+    }
+}
---
+++ Refaktorert_kode_v1.1/NyesteKode/SnakeMess/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace SnakeMess{
+    internal class Game
+    {
+	//	public static bool runGame = true;
+		public static bool RunGame { get; set; }
+		// Henter fila med highscore.
+		private static string _getHighscoreFromFile = File.ReadAllText(@"..\..\score.txt");
+		// Score player har nå.
+	    public static int LastHighscore { get; set; }
+	    
+	    public static void Main(string[] arguments) {
+		    // Oppgave 1
+   //     OriginalGame game = new OriginalGame();
+   //     game.runOriginlGame();
+
+
+	
+			
+			  
+			//	 Oppgave 2
+
+			LastHighscore = Convert.ToInt32(_getHighscoreFromFile);
+			// Starter det første spillet
+			RunGame = true;
+			Menu.GameStartMenu();
+		    
+			while (RunGame)  {
+				var gameEngine = new GameEngine();
+				gameEngine.RunGame();
+				RunGame = Menu.GameOverMenu(gameEngine);
+			} 
+        }
+    }
+}
+
